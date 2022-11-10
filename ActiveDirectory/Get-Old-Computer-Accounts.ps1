4286b990ec5864f729c8416edca736d93c0eaf96
Maria Jimerson <maria.jimerson88@gmail.com>
2022-11-10 10:30:25 -0500

    code cleanup

--- ActiveDirectory/Get-Old-Computer-Accounts.ps1
+++ ActiveDirectory/Get-Old-Computer-Accounts.ps1
@@ -1,30 +1,30 @@
 ﻿function findOldADComputers () {
 	$aOldComputers = @();
 	$aAllAdComputers = Get-ADComputer -Filter * -Properties LastLogonDate,PasswordLastSet | Where { $_.Enabled -eq $true };
-	foreach ($oAdComputer in $aAllAdComputers) { 
+	foreach ($oAdComputer in $aAllAdComputers) {
 		if ($oAdComputer.lastLogonDate -ne $null) {
-			if ($oAdComputer.lastLogonDate -lt [DateTime]::Now.Subtract([TimeSpan]::FromDays(60))) {
-				if ($oAdComputer.PasswordLastSet -lt [DateTime]::Now.Subtract([TimeSpan]::FromDays(60))) {
+			if ($oAdComputer.lastLogonDate -lt [DateTime]::Now.Subtract([TimeSpan]::FromDays(45))) {
+				if ($oAdComputer.PasswordLastSet -lt [DateTime]::Now.Subtract([TimeSpan]::FromDays(45))) {
 					$aOldComputers += $oAdComputer.Name;
-				}##endif
-			}##endif
-		}##endif
-	}##endforeach
+				}
+			}
+		}
+	}
 	return $aOldComputers;
-}##endfunction
+}
 
-$sOldPcFilePath 	= 'C:\Users\abertram\desktop\projects\ad_cleanup\Get-Old-Ad-Accounts-Files\old_computer_accounts.txt';
-$sOnlinePcFilePath 	= 'C:\Users\abertram\desktop\projects\ad_cleanup\Get-Old-Ad-Accounts-Files\online_pcs.txt';
+$sOldPcFilePath 	= 'C:\temp\ad_cleanup\Get-Old-Ad-Accounts-Files\old_computer_accounts.txt';
+$sOnlinePcFilePath 	= 'C:\temp\ad_cleanup\Get-Old-Ad-Accounts-Files\online_pcs.txt';
 
 if (Test-Path $sOnlinePcFilePath) {
 	$aPastOnlinePcs = Get-Content $sOnlinePcFilePath;
 } else {
 	$aPastOnlinePcs = @();
-}##endif
+}
 
 if (Test-Path $sOldPcFilePath) {
 	Remove-Item $sOldPcFilePath -Force
-}##endif
+}
 
 $aCurrentOldPcs = findOldAdComputers;
 
@@ -37,15 +37,15 @@ foreach ($i in $aDnsQueryResults) {
 			if ($aPastOnlinePcs -notcontains $sPc) { ## The PC has never been shown to be online
 				Write-Debug "$sPc has a DNS record but is offline";
 				Add-Content $sOldPcFilePath $sPc;
-			}##endif
+			}
 		} else {
 			Write-Debug "$sPc has a DNS record and is online";
 			if ($aPastOnlinePcs -notcontains $sPc) {
 				Add-Content $sOnlinePcFilePath $sPc;
-			}##endif
-		}##endif
+			}
+		}
 	} else {
 		Write-Debug "$sPc has no DNS record"
 		Add-Content $sOldPcFilePath $sPc;
-	}##endif
-}##endforeach
+	}
+}
